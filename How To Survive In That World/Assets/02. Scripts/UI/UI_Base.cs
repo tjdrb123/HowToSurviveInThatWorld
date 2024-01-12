da4bc7b2b0f8ac8cb79dc43b50e79cfb3fc8601e
박정혁 <146924502+jeonghyeok2@users.noreply.github.com>
2024-01-12 18:27:06 +0900

    HGZ-9 [✨️] - UI Base System
    # UI_Base 스크립트 추가
    - UI_Base를 만들어 Bind 와 Get으로 Ui를 동작할 수 있게 만들었습니다
    - UI_Base를 상속시켜 원하는 동작을 위해 Bind로 묶고 Get으로 가져와 작동하게 할 수 있음
    # Util, Extensions 스크립트 추가
    - Util에서는 FindChild함수를 만들어 원하는 타입의 오브젝트를 하이어라키 창에서 찾아주고
    - Extensions에 FindChild로 확장 메서드를 만들어 gameObjcet에서 편리하게 사용할 수 있도록 만들었습니다.

---
+++ How To Survive In That World/Assets/02. Scripts/UI/UI_Base.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Base : MonoBehaviour
+{
+    //Dictionary자식에 있는 Type과 타입에 해당하는 오브젝트를 관리합니다.
+    private Dictionary<Type, UnityEngine.Object[]> _objects = new();
+
+    private bool _initialized = false;
+
+    protected virtual void OnEnable()
+    {
+        Initialize();
+    }
+
+    //초기화를 했는지 안했는지 체크하는 함수입니다.
+    //여기서 Protected가 아닌 public으로 하는 이유는 초기화를 다른 클래스에서도 하기 위함입니다.
+    public virtual bool Initialize()
+    {
+        if (_initialized == true)
+            return false;
+
+        _initialized = true;
+        return true;
+    }
+
+    private void Bind<T>(Type type) where T : UnityEngine.Object  
+    {
+        string[] names = Enum.GetNames(type); //type의 값들을 다 가져와서 사용함
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length]; //type의 값들의 크기만큼 배열크기 지정
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i] = typeof(T) == typeof(GameObject) ? this.gameObject.FindChild(names[i]) : gameObject.FindChild<T>(names[i]);
+        }
+        _objects.Add(typeof(T), objects);
+    }
+
+    //Bind를 이용해서 dictionary에 값들을 묶어서 저장합니다.
+    protected void BindObject(Type type) => Bind<GameObject>(type);
+    protected void BindText(Type type) => Bind<TextMeshProUGUI>(type);
+    protected void BindButton(Type type) => Bind<Button>(type);
+    protected void BindImage(Type type) => Bind<Image>(type);
+
+
+    private T Get<T>(int index) where T : UnityEngine.Object
+    {
+        if (!_objects.TryGetValue(typeof(T), out UnityEngine.Object[] objs))
+            return null;
+        if (index < 0 && index >= _objects.Count)
+            return null;
+        return objs[index] as T;
+    }
+
+    protected GameObject GetObject(int index) => Get<GameObject>(index);
+    protected TextMeshProUGUI GetText(int index) => Get<TextMeshProUGUI>(index);
+    protected Button GetButton(int index) => Get<Button>(index);
+    protected Image GetImage(int index) => Get<Image>(index);
+
+}
---
+++ How To Survive In That World/Assets/02. Scripts/UI/Utilities/Util.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Util
+{
+    //제네릭으로 값을 받아서 내가 원하는 컴퍼넌트를 찾은 후 리턴한다.
+    public static T FindChild<T>(GameObject go, string name = null) where T : UnityEngine.Object
+    {   
+        if (go == null)
+            return null;
+        //오브젝트의 비활성화된 자식들까지 검사하여 T에 해당하는 오브젝트를 가져옴
+        T[] components = go.GetComponentsInChildren<T>(true);
+        if (string.IsNullOrEmpty(name)) //이름이 null값이면 그냥 첫번째의 값을 가져오고
+            return components[0];
+        else
+            return components.Where(x => x.name == name).FirstOrDefault(); //components에 들어있는 이름과 name 맞으면 값을 가져오거나 없으면 null
+    }
+
+    //GameObject는 컴퍼넌트로 찾을 수 없기 때문에 Transform으로 변환 후 검사하여 전달한다.
+    public static GameObject FindChild(GameObject go, string name = null) 
+    {
+        Transform transform = FindChild<Transform>(go, name);
+        if (transform == null) 
+            return null;
+        return transform.gameObject;
+    }
+}
