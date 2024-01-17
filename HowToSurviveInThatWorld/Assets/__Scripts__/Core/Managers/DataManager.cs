c024b091f9e88473f970d420c9096ce84edd869c
장성규 <zjarhd_tjdrb@naver.com>
2024-01-17 10:33:45 +0900

    HGZ-6 [✨️] - Data Manager Settings
    1. 폴더 추가
            Asset/@Data/CSV
            Asset/@Data/JSON
    
    2. Literals.cs
            # region - DataFile Path 추가
    
    3. Newtonsoft json 패키지 설치
    
    4. DataManager.cs
            csv - json 데이터 변환

---
+++ HowToSurviveInThatWorld/Assets/__Scripts__/Core/Managers/DataManager.cs
@@ -0,0 +1,45 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataManager
+{
+    //csv -> json
+            public void CsvToJson(string fileName, string path = Literals.CSV_PATH)
+            {
+                string fullPath = $"{path}{fileName}.csv";
+    
+                if (!FindFile(fullPath))
+                    return;
+    
+                string[] csvLines = File.ReadAllLines(fullPath);
+    
+                List<Dictionary<string, object>> dataList = new List<Dictionary<string, object>>();
+                string[] headers = csvLines[0].Split(',');
+                for (int i = 1; i < csvLines.Length; i++)
+                {
+                    string[] values = csvLines[i].Split(',');
+                    Dictionary<string, object> dataEntry = new Dictionary<string, object>();
+    
+                    for (int j = 0; j < headers.Length && j < values.Length; j++)
+                    {
+                        dataEntry[headers[j]] = values[j];
+                    }
+    
+                    dataList.Add(dataEntry);
+                }
+    
+                string jsonSting = JsonConvert.SerializeObject(dataList, Formatting.Indented);
+                File.WriteAllText($"{Literals.JSON_PATH}{fileName}.json", jsonSting);
+            }
+    
+            private bool FindFile(string path)
+            {
+                if (File.Exists(path))
+                    return true;
+    
+                Debug.Log($"File not found, Path : {path}");
+                return false;
+            }
+}
