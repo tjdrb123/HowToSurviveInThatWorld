11d4abc62f865a0614b036724621a99ec92cfd3f
GreatJun <ljun970@naver.com>
2024-01-21 19:04:14 +0900

    HGZ-22 [✨️] - EnemyBasicBT, Region Setting
    # EnemyBasicBT - Region Setting
    - 작성할 Action 노드들을 각 분기에 맞게 region으로 묶어서 정리하였습니다.

--- HowToSurviveInThatWorld/Assets/__Scripts__/__Core__/BehaviorTree/EnemyBasicBT.cs
+++ HowToSurviveInThatWorld/Assets/__Scripts__/__Core__/BehaviorTree/EnemyBasicBT.cs
@@ -7,9 +7,16 @@ using UnityEngine;
 public class EnemyBasicBT : MonoBehaviour
 {
     #region Global Variable
+
+    [Header("Distance")] 
+    [SerializeField] 
+    private float _detectDistance;
     
     private DataContext _enemyData;
     private BehaviorTreeRunner _btRunner;
+    
+
+    
 
     #endregion
     
@@ -87,6 +94,41 @@ public class EnemyBasicBT : MonoBehaviour
     
 
     # region Action(Leaf) Nodes
+
+    #region Detect Player Node
+
+    // 범위안에 적이 있는가? 
+    // 범위 거리 체크할 Distance,범위 안에 있다면 플레이어의 위치 정보를 가져올 Transform
+    // 이 쪽에서 플레이어가 존재시에 Transform에 플레이어 정보를 저장한다.
+    // 만약 Transform != null 이라면 할당하지 않고, null 일때에만 할당한다.
+    // 플레이어가 감지 범위 밖으로 나간다면 Transform을 null으로 만들어준다.
+    // 이런식으로 최대한 Physics2D.OverlapCircleAll 를 적게 사용해야 불필요한 연산을 줄일 수 있다.
+
+    #endregion
+
+    #region Correct Destination Check & Patrol/Idle Node
+
+    // 올바른 목적지 체크
+    // 목적지 도착했는가?
+    // 대기시간이 남아있는가?
+    // 이동로직
+
+    #endregion
+
+    #region Attack Check/Excute Node
+
+    // 공격중인가?
+    // 공격범위 안에 적이 있는가?
+    // 공격 실행
+
+    #endregion
+
+    #region Tracking Node
+
+    // 추적 로직
+
+    #endregion
+
     private INode.E_NodeState Test1()
     {
         return INode.E_NodeState.ENS_Running;
