351bcf5845538ef5c326f968a4d26901954c2af2
장성규 <zjarhd_tjdrb@naver.com>
2024-01-25 11:57:51 +0900

    HGZ-33 [🛠️] - PlayerFSM
    스크립트 정리

--- HowToSurviveInThatWorld/Assets/__Scripts__/__Core__/FSM/PlayerFSM.cs
+++ HowToSurviveInThatWorld/Assets/__Scripts__/__Core__/FSM/PlayerFSM.cs
@@ -1,36 +1,19 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
-
-#region PlayerFSM Enum
-
-public enum E_Locations
-{
-    Dungeon = 1,
-    Village,
-};
-
-public enum E_PlayerStates
-{
-    NormallyState,
-    GlobalState,
-}
-
-#endregion
 
 public class PlayerFSM : MonoBehaviour
 {
     #region Field
 
     // Player가 가지고 있는 모든 상태, 현재 상태
-    private Dictionary<E_PlayerStates, State<PlayerFSM>> _states;
+    private Dictionary<E_Player_States, State<PlayerFSM>> _states;
     private StateMachine<PlayerFSM> _stateMachine;
     
     #endregion
 
     #region Properties
     
-    public E_PlayerStates CurrentState // 현재 상태
+    public E_Player_States CurrentState // 현재 상태
     {
         get;
         private set;
@@ -42,18 +25,18 @@ public class PlayerFSM : MonoBehaviour
     public void Awake()
     {
         // Player가 가질 수 없는 상태 개수만큼 메모리 할당, 각 상태에 클래스 메모리 할당
-        _states = new Dictionary<E_PlayerStates, State<PlayerFSM>>
+        _states = new Dictionary<E_Player_States, State<PlayerFSM>>
         {
-            {E_PlayerStates.NormallyState, new NormallyState()},
-            {E_PlayerStates.GlobalState, new GlobalState()}
+            {E_Player_States.NormallyState, new NormallyState()},
+            {E_Player_States.GlobalState, new GlobalState()}
         };
         
         // 상태를 관리하는 StateMachine에 메모리를 할당하고, 첫 상태를 설정
         _stateMachine = new StateMachine<PlayerFSM>();
-        _stateMachine.Setup(this, _states[E_PlayerStates.NormallyState]);
+        _stateMachine.Setup(this, _states[E_Player_States.NormallyState]);
         
         // 전역 상태 설정
-        _stateMachine.SetGlobalState(_states[E_PlayerStates.GlobalState]);
+        _stateMachine.SetGlobalState(_states[E_Player_States.GlobalState]);
         
         // 위에처럼 나머지 필드 데이터 셋업
         /*
@@ -71,7 +54,7 @@ public class PlayerFSM : MonoBehaviour
         _stateMachine.ExecuteFixedUpdate();
     }
 
-    public void ChangeState(E_PlayerStates newState)
+    public void ChangeState(E_Player_States newState)
     {
         CurrentState = newState; // 상태가 바뀌면 새로운 상태가 현재상태로
         
--- HowToSurviveInThatWorld/Assets/__Scripts__/__Core__/FSM/State.cs
+++ HowToSurviveInThatWorld/Assets/__Scripts__/__Core__/FSM/State.cs
@@ -1,7 +1,7 @@
 // 모든 상태가 상속받는 기반 클래스
 public abstract class State<T> where T : class
 {
-    /// 해당 상태를 시작할 때 1회 호출 
+    // 해당 상태를 시작할 때 1회 호출 
     public abstract void Enter(T entity);
     
     // 해당 상태를 업데이트할 때, 매 프레임 호출
@@ -15,9 +15,7 @@ public abstract class State<T> where T : class
     {
         // 물리 연산 관련 업데이트 로직
     }
-
-    /// <summary>
-    /// 해당 상태를 종료할 때 1회 호출 
-    /// </summary>
+    
+    // 해당 상태를 종료할 때 1회 호출 
     public abstract void Exit(T entity);
 }
