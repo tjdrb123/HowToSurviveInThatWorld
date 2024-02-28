0188c0048ecca99d5f9284f977ae87e9c928cf06
박정혁 <146924502+jeonghyeok2@users.noreply.github.com>
2024-02-28 10:33:51 +0900

    HGZ-66 [✨️] - UI작업
    ## Setting_UI
    - 조이스틱을 변경할 수 있도록 추가하였습니다.
    - 설정창으로 버튼을 옮겨서 TestButton 스크립트는 삭제하였습니다.

--- HowToSurviveInThatWorld/Assets/__Scripts__/UI/UI_Popup/UI_SettingPopUp.cs
+++ HowToSurviveInThatWorld/Assets/__Scripts__/UI/UI_Popup/UI_SettingPopUp.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class UI_SettingPopUp : UI_Popup
@@ -10,34 +11,44 @@ public class UI_SettingPopUp : UI_Popup
         Close_Button,
         ON_Button,
         OFF_Button,
+        Fixed_Button,
+        Floating_Button
     }
     enum E_Object
     {
         ONCheckImage,
         OFFCheckImage,
+        ONJoyStickImage,
+        OFFJoyStickImage,
         BGMSlider,
         SFXSlider,
     }
     private Slider _bgmSlider;
     private Slider _sfxSlider;
+    [SerializeField] private InputUIJoystick joystick;
     public override bool Initialize()
     {
         if (!base.Initialize()) return false;
         BindButton(typeof(E_Button));
         BindObject(typeof(E_Object));
         AudioMute(Manager_Sound.instance.IsMute);
-        //¥›±‚ πˆ∆∞
+        //Îã´Í∏∞ Î≤ÑÌäº
         GetButton((int)E_Button.Close_Button).onClick.AddListener(() => ClosePopup());
-        //¿Ωº“∞≈ πˆ∆∞
+        //ÏùåÏÜåÍ±∞ Î≤ÑÌäº
         GetButton((int)E_Button.ON_Button).onClick.AddListener(() => AudioMute(false)); 
         GetButton((int)E_Button.OFF_Button).onClick.AddListener(() => AudioMute(true));
-        //ΩΩ∂Û¿Ã¥ı
+        //Ïä¨ÎùºÏù¥Îçî
         _bgmSlider = GetObject((int)E_Object.BGMSlider).GetComponent<Slider>();
         _sfxSlider = GetObject((int)E_Object.SFXSlider).GetComponent<Slider>();
         _bgmSlider.value = Manager_Sound.instance.BGMVolume;
         _sfxSlider.value = Manager_Sound.instance.SFXVolume;
         _bgmSlider.onValueChanged.AddListener((volume) => AudioVolume(volume, 1));
         _sfxSlider.onValueChanged.AddListener((volume) => AudioVolume(volume, 2));
+        //Ï°∞Ïù¥Ïä§Ìã±
+        joystick = GameObject.Find("JoystickArea").GetComponent<InputUIJoystick>();
+        JoyStick((int)joystick.JoystickType, joystick.JoystickType == InputUIJoystick.E_JoystickType.Fixed);
+        GetButton((int)E_Button.Fixed_Button).onClick.AddListener(() => JoyStick(0, true));
+        GetButton((int)E_Button.Floating_Button).onClick.AddListener(() => JoyStick(1, false));
         return true;
     }
 
@@ -50,5 +61,14 @@ public class UI_SettingPopUp : UI_Popup
     private void AudioVolume(float volume, int num)
     {
         Manager_Sound.instance.AudioVolume(volume, num);
+    }
+    private void JoyStick(int index, bool isMove)
+    {
+        if (index == 0)
+            joystick.SetJoystickMode(InputUIJoystick.E_JoystickType.Fixed);
+        else
+            joystick.SetJoystickMode(InputUIJoystick.E_JoystickType.Floating);
+        GetObject((int)E_Object.ONJoyStickImage).SetActive(isMove);
+        GetObject((int)E_Object.OFFJoyStickImage).SetActive(!isMove);
     }
 }
