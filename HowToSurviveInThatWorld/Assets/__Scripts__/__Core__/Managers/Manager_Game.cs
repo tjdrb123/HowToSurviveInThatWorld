35b7aa0569838ebe7ee85d6617acf804c8afdee6
장성규 <zjarhd_tjdrb@naver.com>
2024-03-01 17:25:48 +0900

    EnemySpawner system
    간단한 스폰 시스템 입니다.
    고정된 스폰 포인트에서 5초 주기로 최대 30마리까지 좀비가 스폰됩니다.

--- HowToSurviveInThatWorld/Assets/__Scripts__/__Core__/Managers/Manager_Game.cs
+++ HowToSurviveInThatWorld/Assets/__Scripts__/__Core__/Managers/Manager_Game.cs
@@ -1,4 +1,7 @@
 
+using Unity.VisualScripting;
+using UnityEngine;
+
 /// <summary>
 /// # Game Manager
 ///   - 추 후 필요로 하게 되면 사용 (현재 : 설계 필요)
@@ -7,3 +10,5 @@ public class Manager_Game
 {
     
 }
+
+
